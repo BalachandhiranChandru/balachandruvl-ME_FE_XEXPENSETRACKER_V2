430cb5476718c8415cd711df703abb32d987624a
BalachandhiranChandru <119665136+BalachandhiranChandru@users.noreply.github.com>
2025-11-13 18:39:50 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import Home from './pages/Home/Home'
-import { SnackbarProvider } from 'notistack';
+// import Home from './pages/Home/Home'
+import Home from './components/Home/Home'
+
+// import { SnackbarProvider } from 'notistack';
 
 function App() {
   return (
-    <SnackbarProvider >
-      <div>
-        <Home />
-      </div>
-    </SnackbarProvider>
+    <div className="App">
+      <Home />
+      {/* <h1>Welcome to the My-App</h1> */}
+    </div>
+    // <SnackbarProvider >
+    //   <div>
+    //     <Home />
+    //   </div>
+    // </SnackbarProvider>
   );
 }
 
